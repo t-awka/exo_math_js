da274a0059740ca27b2ba34ffc0cee05be48fb75
t-awka <80962656+t-awka@users.noreply.github.com>
2021-04-29 15:08:45 +0200

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+let nombre = 8.56
+// 2
+console.log(Math.round(nombre));
+// 3
+console.log(Math.ceil(nombre));
+// 4
+console.log(Math.floor(nombre));
+// 5
+console.log(Math.trunc(nombre));
+// let chiffre = Math.floor(nombre);
+// console.log(chiffre.toFixed(0));
+// 6
+console.log(Math.random());
+// 7
+console.log(Math.random() * 100);
+// 8
+console.log(Math.floor(Math.random() * 100));
+// let numero = Math.random() * 100;
+// console.log(numero.toFixed());
+// 9
+console.log(Math.pow(8, 2));
+// 10
+console.log(Math.sqrt(9));
+// 11
+console.log(Math.abs(-1));
+// 12
+console.log(Math.max(-2, 1000, 8, 57));
+// 13
+console.log(Math.min(-2, 1000, 8, 57));
